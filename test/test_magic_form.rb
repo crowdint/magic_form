6f366c1de9387b41a9dbd45a2cea43712e6807ae
Luis Galaviz <luis.galaviz@crowdint.com>
2010-12-02 16:58:21 -0600

    A test was missing. Now all the tests are runing properly.

--- test/test_magic_form.rb
+++ test/test_magic_form.rb
@@ -17,7 +17,11 @@ class MagicFormTest < ActionView::TestCase
   context "having a product create a form with the Product attributes" do
 
     should "work with no options" do
-      assert_equal "",
+      assert_equal (form_for @product do |f|
+                      concat(f.label :name)
+                      concat(f.text_field :name)
+                      concat(f.submit)
+                    end),
                     magic_form(@product)
     end
 
